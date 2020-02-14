ed2a1ad5e6550f1d8bed6e16549fb87ef1a40970
Fanwei Kong <fanwei_kong@berkeley.edu>
2020-02-14 14:30:54 -0800

    Updates to work on cluster

--- examples/lv_deformable.py
+++ examples/lv_deformable.py
@@ -1,19 +1,12 @@
 from functools import partial
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from pycpd import deformable_registration
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../pycpd'))
+import glob
+from deformable_registration import deformable_registration
 import numpy as np
 import time
-#import vtk
-
-#def visualize(iteration, error, X, Y, ax):
-#    plt.cla()
-#    ax.scatter(X[:,0],  X[:,1], X[:,2], color='red', label='Target')
-#    ax.scatter(Y[:,0],  Y[:,1], Y[:,2], color='blue', label='Source')
-#    ax.text2D(0.87, 0.92, 'Iteration: {:d}\nError: {:06.4f}'.format(iteration, error), horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, fontsize='x-large')
-#    ax.legend(loc='upper left', fontsize='x-large')
-#    plt.draw()
-#    plt.pause(0.001)
+from mpi4py import MPI
 
 def readPD(fn):
     import vtk
@@ -39,18 +32,37 @@ def writePD(fn, pts, poly):
 def main():
     #X, _, mean_X = readPD('/Users/fanweikong/Documents/Modeling/SurfaceModeling/Label_based/MACS40282_20150504/surfaces/phase6_deci.nii.vtk')
     #Y, poly, mean_Y  = readPD('/Users/fanweikong/Documents/Modeling/SurfaceModeling/Label_based/MACS40282_20150504/surfaces/phase9_deci.nii.vtk')
-    fn_X = ''
-    fn_Y = ''
-    fn_Y_out = ''
-    X = np.load(fn_X)
-    Y = np.load(fn_Y)
-    X_mean = np.mean(X, axis=0)
-    Y_mean = np.mean(Y, axis=0)
-    reg = deformable_registration(**{ 'X': X-X_mean, 'Y': Y-Y_mean})
-    TY, _ = reg.register()
-    TY += Y_mean
-    np.save(fn_Y_out, TY)
-    #writePD('/Users/fanweikong/Downloads/phase9_6.vtk', TY, poly)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', help='Input directory name')
+    parser.add_argument('--output', help='Output directory name')
+    args = parser.parse_args()
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    total = comm.Get_size()
+    
+    fns = glob.glob(os.path.join(args.input, '*.npy'))
+    
+    num_vol_per_core = int(np.floor(len(fns)/total))
+    extra = len(fns) % total
+    vol_ids = list(range(rank*num_vol_per_core,(rank+1)*num_vol_per_core))
+    if rank < extra:
+        vol_ids.append(len(fns)-1-rank)
+
+    for i in vol_ids:
+        fn_X = fns[0]
+        fn_Y = fns[i]
+        fn_Y_out = os.path.join(args.output, os.path.basename(fn_Y))
+        X = np.load(fn_X)
+        Y = np.load(fn_Y)
+        X_mean = np.mean(X, axis=0)
+        Y_mean = np.mean(Y, axis=0)
+        reg = deformable_registration(**{ 'X': X-X_mean, 'Y': Y-Y_mean})
+        TY, _ = reg.register()
+        TY += Y_mean
+        np.save(fn_Y_out, TY)
+        #writePD('/Users/fanweikong/Downloads/phase9_6.vtk', TY, poly)
 
 
 if __name__ == '__main__':
