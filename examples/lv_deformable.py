9a86f6513e815c8c6c5ecb581d75e1337d5d5933
Fanwei Kong <fanwei_kong@berkeley.edu>
2020-02-19 16:04:26 -0800

    Change of settings

--- examples/lv_deformable.py
+++ examples/lv_deformable.py
@@ -43,18 +43,23 @@ def main():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     total = comm.Get_size()
-    
+    if rank ==0:
+        try:
+            os.makedirs(args.output)
+        except Exception as e: print(e)
     fns = sorted(glob.glob(os.path.join(args.input, '*.npy')))
-    print("Template: ", fns[0])
     num_vol_per_core = int(np.floor(len(fns)/total))
     extra = len(fns) % total
     vol_ids = list(range(rank*num_vol_per_core,(rank+1)*num_vol_per_core))
     if rank < extra:
         vol_ids.append(len(fns)-1-rank)
+   
+    ID = 9
+    print("Template: ", fns[ID])
 
     for i in vol_ids:
         fn_X = fns[i]
-        fn_Y = fns[0]
+        fn_Y = fns[ID]
         fn_X_out = os.path.join(args.output, os.path.basename(fn_X))
         X = np.load(fn_X)
         Y = np.load(fn_Y)
@@ -66,9 +71,7 @@ def main():
         Y, _ = reg.register()
         reg = affine_registration(**{'X':X, 'Y':Y})
         Y, _ = reg.register()
-        reg = deformable_registration(**{ 'X': X, 'Y': Y})
-        Y, _ = reg.register(rank=rank)
-        reg = deformable_registration(**{ 'X': X, 'Y': Y})
+        reg = deformable_registration(alpha=10, beta=40,**{ 'X': X, 'Y': Y})
         TY, _ = reg.register(rank=rank)
         TY += X_mean
         np.save(fn_X_out, TY)
